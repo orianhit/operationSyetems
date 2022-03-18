8ffbbe03b4d8308f572de9253b84be4e80bcf675
orian <orianhit@hit.ac.il>
2022-03-18 09:25:42 +0200

    fix indent

--- main.c
+++ main.c
@@ -18,15 +18,14 @@ void printPrompt() {
      printf("%d-%02d-%02d %02d:%02d:%02d %s $ %s : \n", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec, hostname, cwd);
 }
 
-char *concatenate(char *a, char *b, char *c)
-{
-  int size = strlen(a) + strlen(b) + strlen(c) + 1;
-  char *str = malloc(size);
-  strcpy (str, a);
-  strcat (str, b);
-  strcat (str, c); 
+char *concatenate(char *a, char *b, char *c){
+     int size = strlen(a) + strlen(b) + strlen(c) + 1;
+     char *str = malloc(size);
+     strcpy (str, a);
+     strcat (str, b);
+     strcat (str, c); 
 
-  return str;
+     return str;
 }
 
 int runcmd(char *cmd) {
@@ -36,20 +35,13 @@ int runcmd(char *cmd) {
 
      if (pid == -1) {
           printf("failed to create proc \n");
-          return 0;
      } else if (pid == 0) {
           execv(cmd, argv_for_program);
           _exit(127);
      } else {
           int child_pid = wait(&child_status);
-          if (child_pid > 0) {
-               if(WIFEXITED(child_status)) {
-                    if (WEXITSTATUS(child_status) != 127) {
-                         return 1;
-                    }
-               } else {
-                    return 0;
-               }
+          if (child_pid > 0 && WIFEXITED(child_status) && WEXITSTATUS(child_status) != 127) {
+               return 1;
           }
      }
      return 0;
